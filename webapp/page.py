540c4323af63bc5c1e9212cb9bf375b812c8f894
Ione Axelrod <malia@Malias-Macbook-Pro.local>
2024-02-20 21:45:30 -0800

    Fixed gitignore

--- webapp/page.py
+++ webapp/page.py
@@ -1,7 +1,10 @@
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for
+    Blueprint, render_template, send_file
 )
 from werkzeug.exceptions import abort
+
+RESUME_PATH = 'static/resume.pdf'
+
 
 bp = Blueprint('page', __name__)
 
@@ -11,7 +14,6 @@ def index():
     return render_template("homepage.html")
 
 
-@app.route('/resume')
+@bp.route('/resume')
 def show_resume():
-    with open(RESUME_PATH, READ_BYTES) as static_file:
-        return send_file(static_file, attachment_filename='resume.pdf')
+    return send_file(RESUME_PATH, attachment_filename='resume.pdf')
